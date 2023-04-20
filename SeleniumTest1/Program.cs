248238d5a7ab1c75d6d29344d290bded5d4192fb
Gabriel Azaph <gabrielazaph@hotmail.com>
2023-04-19 22:19:17 -0300

    Tests With Latest Version Selenium WebDriver

--- SeleniumTest1/Program.cs
+++ SeleniumTest1/Program.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V109.WebAudio;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +16,34 @@ namespace SeleniumTest1
         {
             Console.WriteLine("Tests Selenium!");
             // TEST
+
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://github.com/GabrielAzaph");
+            if (driver.Url.Contains("GabrielAzaph"))
+            {
+                //TestingPrintDriver(driver);
+                //var dvTSession = driver.GetDevToolsSession();
+
+            }
+        }
+
+        private static void TestingPrintDriver(ChromeDriver driver)
+        {
+            var optionsPrint = new PrintOptions();
+            optionsPrint.AddPageToPrint(1);
+
+
+            var print = driver.Print(optionsPrint);
+            var pagePrint = print;
+
+            try
+            {
+                File.WriteAllBytes(@"C:\Users\Public\Pictures\PrintPage2.jpeg", print.AsByteArray);
+            }
+            catch (Exception)
+            {
+
+            }
         }
     }
 }
